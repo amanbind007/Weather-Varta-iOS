d81824343ff9786fbc31a768c549f470b3b7dcef
Aman <amanbind007@gmail.com>
2024-02-10 21:46:04 +0530

    first commit

---
+++ Weather Varta/Weather_VartaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_VartaApp.swift
+//  Weather Varta
+//
+//  Created by Aman Bind on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_VartaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
