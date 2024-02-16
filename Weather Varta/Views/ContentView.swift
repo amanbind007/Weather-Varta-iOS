35de9f464f37fa9e5c8fbf32222691bbe89f3f86
Aman <amanbind007@gmail.com>
2024-02-16 22:19:26 +0530

    Updated UI and added theme support

--- Weather Varta/Views/ContentView.swift
+++ Weather Varta/Views/ContentView.swift
@@ -8,48 +8,128 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchCity: String = ""
     
-    @State private var search : String = ""
+    @Environment(\.colorScheme) private var theme
     
     var body: some View {
         VStack {
             ScrollView {
-                
-                Image(systemName: "cloud.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                
-                Text("Haze/Cloudy/Whatever")
+                HStack {
+                    TextField("Enter your City", text: $searchCity)
+                        .foregroundStyle(.pink)
+                        .textFieldStyle(.roundedBorder)
                     
-                HStack(alignment: .top, content: {
-                    Text("35")
-                        
-                        .font(.system(size: 100))
-                        
-                    Text("℃")
-                        .offset(y:18)
-                })
-                
-                HStack{
-                    Text("High:")
-                    Text("23℃")
-                    
-                    Text("Low:")
-                    Text("8℃")
+                    Button(action: {}, label: {
+                        Image(systemName: "location.circle.fill").imageScale(.large)
+                            .foregroundStyle(.blue)
+                    })
                 }
                 
+                Spacer(minLength: 15)
+                
+                VStack {
+                    Image(systemName: "cloud.rain")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 70, height: 70)
+                    
+                    Text("Haze/Cloudy/Whatever")
+                    
+                    HStack(alignment: .top, content: {
+                        Text("35")
+                        
+                            .font(.system(size: 80))
+                        
+                        Text("℃")
+                            .offset(y: 18)
+                    })
+                    
+                    HStack {
+                        Text("High:")
+                        Text("23℃")
+                        
+                        Text("Low:")
+                        Text("8℃")
+                    }
+                }
+                .frame(width: 356, height: 270)
+                .background(Color("background_card_big"))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer(minLength: 15)
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(systemName: "humidity")
+                            
+                            VStack {
+                                Text("fsdfsdfsf")
+                                Text("232342424")
+                            }
+                        }
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer(minLength: 15)
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "wind")
+                            
+                            VStack {
+                                Text("fsdfsdfsf")
+                                Text("232342424")
+                            }
+                        }
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+                Spacer(minLength: 15)
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(systemName: "humidity")
+                            
+                            VStack {
+                                Text("fsdfsdfsf")
+                                Text("232342424")
+                            }
+                        }
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer(minLength: 15)
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "humidity")
+                            
+                            VStack {
+                                Text("fsdfsdfsf")
+                                Text("232342424")
+                            }
+                        }
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
             }
         }
         .navigationTitle("City Name")
-        .searchable(text: $search, prompt: "Enter you city")
         .padding()
-        
+        .background(Color("background"))
     }
 }
 
 #Preview {
-    NavigationStack{
+    NavigationStack {
         ContentView()
     }
 }
--- Weather Varta/Weather_VartaApp.swift
+++ Weather Varta/Weather_VartaApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Weather_VartaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
