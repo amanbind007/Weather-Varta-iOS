77130bbb2a8bb31f3c71fb7121255d065e0d5984
Aman <amanbind007@gmail.com>
2024-02-23 23:00:00 +0530

    Updated the small fist detail cards in the home view

--- Weather Varta/Views/WeatherView.swift
+++ Weather Varta/Views/WeatherView.swift
@@ -70,26 +70,25 @@ struct WeatherView: View {
                             .offset(y: 15)
                                 
                     })
-                    .offset(y:20)
+                    .offset(y: 20)
                     
                     Text(networkManager.result?.weather_desc ?? "Clear")
                         .offset(y: 10)
                         
-                    
-                    HStack{
+                    HStack {
                         Spacer()
                         VStack {
-                            HStack{
+                            HStack {
                                 Text("MAX:")
                                 Text(networkManager.result?.max_temp_string ?? "25℃")
                             }
                             
-                            HStack{
+                            HStack {
                                 Text("MIN:")
                                 Text(networkManager.result?.min_temp_string ?? "25℃")
                             }
                         }
-                        .offset(y:5)
+                        .offset(y: 5)
                         
                         Spacer()
                         HStack {
@@ -107,7 +106,6 @@ struct WeatherView: View {
                             }
                         }
                         Spacer()
-                        
                     }
                 }
                 .frame(width: 356, height: 250)
@@ -116,150 +114,20 @@ struct WeatherView: View {
                 Spacer(minLength: 12)
                 
                 HStack {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "eye")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                                
-                            VStack(alignment: .leading) {
-                                HStack{
-                                    Text("10")
-                                        .font(.title2)
-                                    Text("km")
-                                        .font(.footnote)
-                                        .offset(x:-5, y:2)
-                                }
-                                
-                                Text("VISIBILITY")
-                                    .font(.caption2)
-                            }
-                        }.offset(x:20)
-                        
-                        Divider()
-                        HStack {
-                            Image(systemName: "cloud")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                            VStack(alignment: .leading) {
-                                HStack{
-                                    Text("60")
-                                        .font(.title2)
-                                    Text("%")
-                                        .font(.footnote)
-                                        .offset(x:-5, y:2)
-                                }
-                                Text("CLOUDINESS")
-                                    .font(.caption2)
-                            }
-                        }
-                        .offset(x:20)
-                    }
-                    .frame(width: 173, height: 140)
-                    .background(Color("background_card"))
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    
-                    Spacer(minLength: 12)
-                    
                     HStack {
-                        Spacer()
-                        VStack {
-                            Image(systemName: "gauge.with.dots.needle.bottom.50percent")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                           
-                                Text("1019")
-                                    .font(.title2)
-                                    
-                            
-                            
-                                
-                            Text("PRESSURE")
-                                .font(.caption2)
-                        }
-                        Spacer()
-                        Divider()
-                        Spacer()
-                        VStack {
-                            Image(systemName: "humidity.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                            HStack{
-                                Text("50")
-                                    .font(.title2)
-                                    
-                                Text("%")
-                                    .font(.footnote)
-                                    .offset(x:-5, y:2)
-                            }
-                            .offset(y:-5)
-                            
-                            Text("HUMIDITY")
-                                .font(.caption2)
-                        }
-                        Spacer()
-                    }
-                    .frame(width: 173, height: 140)
-                    .background(Color("background_card"))
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                }
-                Spacer(minLength: 12)
-                HStack {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image(systemName: "wind")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                                
-                            VStack(alignment: .leading) {
-                                Text("39 KPH")
-                                    .font(.title2)
-                                Text("WIND SPEED")
-                                    .font(.caption2)
-                            }
-                        }.offset(x:20)
-                        
-                        Divider()
-                        HStack {
-                            Image(systemName: "safari")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                            VStack(alignment: .leading) {
-                                Text("NNE")
-                                    .font(.title2)
-                                Text("WIND DIRECTION")
-                                    .font(.caption2)
-                            }
-                        }
-                        .offset(x:20)
-                    }
-                    .frame(width: 173, height: 140)
-                    .background(Color("background_card"))
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    
-                    Spacer(minLength: 12)
-                    
-                    HStack {
-                        
                         VStack {
                             Image(systemName: "sunrise.fill")
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 25)
                             
-                            HStack{
+                            HStack {
                                 Text("5:30")
                                     .font(.title3)
                                     .offset(y: -5)
                                 Text("PM")
                                     .font(.caption2)
-                                    .offset(x:-5, y:-5)
+                                    .offset(x: -5, y: -5)
                             }
                                 
                             Text("SUNRISE")
@@ -274,19 +142,157 @@ struct WeatherView: View {
                                 .scaledToFit()
                                 .frame(width: 25)
                             
-                            HStack{
+                            HStack {
                                 Text("5:30")
                                     .font(.title3)
                                     .offset(y: -5)
                                 Text("PM")
                                     .font(.caption2)
-                                    .offset(x:-5, y:-5)
+                                    .offset(x: -5, y: -5)
                             }
                             Text("SUNSET")
                                 .font(.caption2)
                         }
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer(minLength: 12)
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "eye")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                                
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("6.0")
+                                        .font(.title2)
+                                    Text("km")
+                                        .font(.footnote)
+                                        .offset(x: -5, y: 2)
+                                }
+                                
+                                Text("VISIBILITY")
+                                    .font(.caption2)
+                            }
+                        }.offset(x: 25)
                         
+                        Divider()
+                        HStack {
+                            Image(systemName: "cloud")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("50")
+                                        .font(.title2)
+                                    Text("%")
+                                        .font(.footnote)
+                                        .offset(x: -5, y: 2)
+                                }
+                                Text("CLOUDINESS")
+                                    .font(.caption2)
+                            }
+                        }
+                        .offset(x: 25)
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+                Spacer(minLength: 12)
+                HStack {
+                    
+                    
+                    
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "humidity")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                                
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("50")
+                                        .font(.title2)
+                                    Text("%")
+                                        .font(.footnote)
+                                        .offset(x: -5, y: 2)
+                                }
+                                
+                                Text("HUMIDITY")
+                                    .font(.caption2)
+                            }
+                        }.offset(x: 25)
                         
+                        Divider()
+                        HStack {
+                            Image(systemName: "gauge")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("1,019")
+                                        .font(.title2)
+                                    Text("hPa")
+                                        .font(.footnote)
+                                        .offset(x: -5, y: 2)
+                                }
+                                Text("PRESSURE")
+                                    .font(.caption2)
+                            }
+                        }
+                        .offset(x: 25)
+                    }
+                    .frame(width: 173, height: 140)
+                    .background(Color("background_card"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer(minLength: 12)
+                    
+                    HStack {
+                        VStack {
+                            Image(systemName: "wind")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                                
+                            HStack {
+                                Text("3.6")
+                                    .font(.title2)
+                                
+                                Text("m/s")
+                                    .font(.footnote)
+                                    .offset(x: -5, y: 2)
+                            }
+                            Text("WIND\nSPEED")
+                                .font(.caption2)
+                                .multilineTextAlignment(.center)
+                                .offset(y:5)
+                        }
+                        
+                        Divider()
+                        VStack(alignment: .center) {
+                            Image(systemName: "safari")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20)
+                            
+                            Text("NNE")
+                                .font(.title2)
+                            Text("WIND\nDIRECTION")
+                                .font(.caption2)
+                                .multilineTextAlignment(.center)
+                                .offset(y:5)
+                        }
                     }
                     .frame(width: 173, height: 140)
                     .background(Color("background_card"))
